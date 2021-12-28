6e036f6accd8ee07ebef7264630dd80dee304df6
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-28 19:31:25 +0530

    state

---
+++ usestate/src/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return <h1>My favorite color is {color}!</h1>
+}
+
+ReactDOM.render(<FavoriteColor />, document.getElementById('root'));
+
+export default FavoriteColor;
